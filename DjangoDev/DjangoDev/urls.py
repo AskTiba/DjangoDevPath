e24aa69f4e2526fb5368bf80f44dace5033da3d5
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-06-18 21:45:18 +0300

    Setup tailwind css

--- DjangoDev/DjangoDev/urls.py
+++ DjangoDev/DjangoDev/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
